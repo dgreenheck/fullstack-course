6e8b99ab13d3f60dcc47be118243e49b2ae06885
Dan Greenheck <dgreenheck@gmail.com>
2022-05-05 22:17:50 -0500

    Added stuff

---
+++ 2_Communicating_with_Server/notes/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'
+import noteService from './services/notes'
+
+noteService
+  .getAll()
+  .then(response => {
+    const notes = response.data
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <App notes={notes} />
+  )
+})
